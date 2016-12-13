c321d8316b428f437a2dd57877bfc11d60b168d7
GuoChen-WHU <496474070@qq.com>
2016-12-13 22:22:13 +0800

    Commit task13 and task17.

---
+++ task17/Gruntfile.js
@@ -0,0 +1,75 @@
+'use strict';
+
+module.exports = function( grunt ) {
+
+  var lrPort = 35729;
+  var lrSnippet = require( 'connect-livereload' )({ port: lrPort });
+
+  var lrMiddleware = function( connect, options ) {
+    return [
+      lrSnippet,
+      connect.static( options.base[0] ),
+      connect.directory( options.base[0] )
+    ];
+  };
+
+  grunt.initConfig({
+
+    connect: {
+      options: {
+        port: 8000,
+        hostname: 'localhost',
+        base: '.'
+      },
+      livereload: {
+        options: {
+          middleware: lrMiddleware
+        }
+      }
+    },
+
+    watch: {
+      livereload: {
+        options: {
+          livereload: lrPort
+        },
+        files: [ 
+          'build/**/*.css',
+          'src/**/*.js',
+          './index.html'
+        ]
+      },
+      
+      build_css: {
+        tasks: [ 'less' ],
+        files: [ 'src/less/*.less' ]
+      },
+
+      jshint: {
+        tasks: [ 'jshint' ],
+        files: [ 'src/js/*.js' ]
+      }
+    },
+
+    less: {
+      compile: {
+        files: {
+          'build/css/compiled.css': 'src/less/*.less'
+        }
+      }
+    },
+
+    jshint: {
+      files: [ 'src/js/**.js' ]
+    }
+
+  });
+
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
+  grunt.loadNpmTasks( 'grunt-contrib-connect' );
+  grunt.loadNpmTasks( 'grunt-contrib-less' );
+  grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+
+  grunt.registerTask( 'live', [ 'connect', 'watch' ] );
+  grunt.registerTask( 'build', [ 'less', 'jshint' ] );
+};
